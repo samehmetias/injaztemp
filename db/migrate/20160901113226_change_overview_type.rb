90e6595277d0aeafc056ede164bb69bf024183d1
minametias <metias.mina@gmail.com>
2016-09-01 13:38:34 +0200

    init

--- db/migrate/20160901113226_change_overview_type.rb
+++ db/migrate/20160901113226_change_overview_type.rb
@@ -1,10 +1,10 @@
 class ChangeOverviewType < ActiveRecord::Migration
   def up
-    change_column :program, :overview, :text
+    change_column :programs, :overview, :text
 end
 def down
     # This might cause trouble if you have strings longer
     # than 255 characters.
-    change_column :program, :overview, :string
+    change_column :programs, :overview, :string
 end
 end
