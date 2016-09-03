9a41ab003704eb7276ad28e78c7eadc2d2919fcb
minametias <metias.mina@gmail.com>
2016-09-03 15:57:55 +0200

    init

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -85,7 +85,7 @@ class EmployeesController < ApplicationController
   def notifyUser
     apn = ApnHelper::Apn.new
     id = 2
-    token = Phone.where(user_xid: id)
+    token = Phone.where(user_id: id)
     puts '++++++++++NotifyUser+++++++++++++'
       puts token
     puts '++++++++++NotifyUser+++++++++++++'
