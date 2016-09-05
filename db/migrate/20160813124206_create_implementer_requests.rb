26b32b3260115e6a1799d115fd88b173b8102947
minametias <metias.mina@gmail.com>
2016-09-05 13:12:30 +0200

    init

--- db/migrate/20160813124206_create_implementer_requests.rb
+++ db/migrate/20160813124206_create_implementer_requests.rb
@@ -2,7 +2,7 @@ class CreateImplementerRequests < ActiveRecord::Migration
   def change
     create_table :implementer_requests do |t|
       t.string :classroom,        default: ""
-      t.date :start_date
+      t.datetime :start_date
       t.integer :duration,        default: 0
       t.integer :school_id,       null: false, default: 0
       t.integer :user_id,         null: false, default: 0
