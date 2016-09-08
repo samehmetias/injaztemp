c8add1ae1e99d052a37dd1d244df79c655dedd02
minametias <metias.mina@gmail.com>
2016-09-08 13:28:43 +0200

    init

--- app/helpers/apn_helper.rb
+++ app/helpers/apn_helper.rb
@@ -1,14 +1,14 @@
 module ApnHelper
   class Apn
 
-  	def sendAlert token, title, sound, is_android
+  	def sendAlert token, title, message, sound, is_android
   		APNS.host = 'gateway.push.apple.com' 
     	APNS.pem  = 'config/certs/apn_production.pem'
       # APNS.pem  = 'config/certs/apn_development.pem'
     	APNS.port = 2195
     	gcm = GCM.new("AIzaSyBbUoVr1Owunr9-vDUPsrPQlsHjEEujgxA")
   		if is_android
-  			options = {data: {title: title, message: ""}}
+  			options = {data: {title: title, message: message}}
             response = gcm.send([token], options)
   		else
   			APNS.send_notification(token, :alert => title, :badge => 1, :sound => sound)
