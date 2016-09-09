0271ff26d459dc578896f228a0fcc4eb83f6bf35
minametias <metias.mina@gmail.com>
2016-09-09 20:43:13 +0200

    init

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -72,6 +72,20 @@ class LessonsController < ApplicationController
     end
   end
 
+  def notifyUser(message,u_id)
+    apn = ApnHelper::Apn.new
+    id = u_id
+    u = Phone.where(user_id: id).first
+    if (!(u.nil?))
+      token = u.token
+      puts '++++++++++NotifyUser+++++++++++++'
+        puts token
+      puts '++++++++++NotifyUser+++++++++++++'
+      apn.delay(:priority => 1).sendAlert(token, "INJAZ Egypt",message,"",true)
+      # render :text => '1'
+    end
+  end
+
   def accept
     @imp = set_lesson
     @imp.accept
