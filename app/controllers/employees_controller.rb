afd6d22daafd0b32a20979f8750e2901746dd12b
minametias <metias.mina@gmail.com>
2016-09-24 18:04:00 +0200

    init

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,6 +1,6 @@
 class EmployeesController < ApplicationController
   before_action :set_employee, only: [:show, :edit, :update, :destroy]
-  before_filter :verify_is_admin 
+  before_filter :verify_is_admin , except: [:show]
   before_filter :authenticate_user!
 
   # GET /employees
