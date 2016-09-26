876ad59c0436e495ddd5c2943a886c6cb9d1f8de
mina sedra <mina.sedra@robustastudio.com>
2016-09-26 08:16:34 +0300

    noti

--- app/helpers/apn_helper.rb
+++ app/helpers/apn_helper.rb
@@ -3,7 +3,7 @@ module ApnHelper
 
   	def sendAlert token, title, message, sound, is_android
   		APNS.host = 'gateway.push.apple.com' 
-    	APNS.pem  = 'config/certs/apn_production.pem'
+    	APNS.pem  = 'config/certs/aps_development.pem'
       # APNS.pem  = 'config/certs/apn_development.pem'
     	APNS.port = 2195
     	gcm = GCM.new("AIzaSyBbUoVr1Owunr9-vDUPsrPQlsHjEEujgxA")
